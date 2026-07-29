1012964eeb9ffe31c29d9bfd0ac54d6b4f46ab41
arunsagarrai01 <arunsagarrai6@gmail.com>
2026-07-29 19:26:52 +0545

    map and  position required parameters

---
+++ map.dart
@@ -0,0 +1,26 @@
+/*
+void main() {
+  String result = joinNames("John", "Doe");
+  print(result);
+}
+
+String joinNames(String name1, String name2) {
+  return name1 + " " + name2;
+}
+*/
+
+// Types of parameters
+// 1. Positional Parameters
+// a. required positional parameters
+
+// a. required positional parameters example
+// should be correct order of values
+// correct order of values(arguments), all values(arg)
+void main() {
+  int result = sum(10, 20, 30);
+  print(result);
+}
+
+int sum(int value1, int value2, int value3) {
+  return value1 + value2 + value3;
+}
