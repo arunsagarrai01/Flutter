5a0c3a226c50bda40a5701bcd14af0a536938dc3
arunsagarrai01 <arunsagarrai6@gmail.com>
2026-07-29 19:31:11 +0545

    optional parameters

--- map.dart
+++ map.dart
@@ -16,6 +16,7 @@ String joinNames(String name1, String name2) {
 // a. required positional parameters example
 // should be correct order of values
 // correct order of values(arguments), all values(arg)
+/*
 void main() {
   int result = sum(10, 20, 30);
   print(result);
@@ -24,3 +25,39 @@ void main() {
 int sum(int value1, int value2, int value3) {
   return value1 + value2 + value3;
 }
+*/
+
+/*
+void main() {
+  // 2. Optional positional parameters
+  // optional positional parameters example
+  int result = multiply(10, 20);
+  print(result);
+}
+
+int multiply(int value1, int value2, [int value3 = 1]) {
+  return value1 * value2 * value3;
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
